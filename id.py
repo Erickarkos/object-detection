b5b6fc7ecd92d1dba23fc94cb65fb01f7d03b20a
Eric Karkos <karkoseric@gmail.com>
2019-04-21 12:04:44 -0600

    Initial commit

---
+++ id.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+print(fashion_mnist)
+
